232f5a2510bb0903b369da7362147bf70f938218
Mento79 <bodyomar79@gmail.com>
2022-01-10 16:09:34 +0200

    web Socket Try

--- Front/src/app/canvas.component.ts
+++
@@ -1,8 +0,0 @@
-import {
-    Component, Input, ElementRef, AfterViewInit, ViewChild
-  } from '@angular/core';
-  import { fromEvent } from 'rxjs';
-  import { switchMap, takeUntil, pairwise } from 'rxjs/operators'
-
- 
-  
